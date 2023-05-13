7b521d8115a7e993681a12a69f1250b87c36d898
Suvalakshmi <107095069+Suvalakshmi-S@users.noreply.github.com>
2023-05-13 08:30:47 +0530

    Add files via upload

---
+++ 19.heap sort.c
@@ -0,0 +1,69 @@
+#include <stdio.h>  
+/* function to heapify a subtree. Here 'i' is the   
+index of root node in array a[], and 'n' is the size of heap. */  
+int i; 
+void heapify(int a[], int n, int i)  
+{  
+    int largest = i; // Initialize largest as root  
+    int left = 2 * i + 1; // left child  
+    int right = 2 * i + 2; // right child  
+    // If left child is larger than root  
+    if (left < n && a[left] > a[largest])  
+        largest = left;  
+    // If right child is larger than root  
+    if (right < n && a[right] > a[largest])  
+        largest = right;  
+    // If root is not largest  
+    if (largest != i) {  
+        // swap a[i] with a[largest]  
+        int temp = a[i];  
+        a[i] = a[largest];  
+        a[largest] = temp;  
+          
+        heapify(a, n, largest);  
+    }  
+}  
+/*Function to implement the heap sort*/  
+void heapSort(int a[], int n)  
+{  
+    for (i = n / 2 - 1; i >= 0; i--)  
+        heapify(a, n, i);  
+    // One by one extract an element from heap  
+    for (i = n - 1; i >= 0; i--) {  
+        /* Move current root element to end*/  
+        // swap a[0] with a[i]  
+        int temp = a[0];  
+        a[0] = a[i];  
+        a[i] = temp;  
+          
+        heapify(a, i, 0);  
+    }  
+}  
+/* function to print the array elements */  
+void printArr(int arr[], int n)  
+{  
+    for ( i = 0; i < n; ++i)  
+    {  
+        printf("%d", arr[i]);  
+        printf(" ");  
+    }  
+          
+}  
+int main()  
+{  
+    int a[] = {48, 10, 23, 43, 28, 26, 1};  
+    int n = sizeof(a) / sizeof(a[0]);  
+    printf("Before sorting array elements are - \n");  
+    printArr(a, n);  
+    heapSort(a, n);  
+    printf("\nAfter sorting array elements are - \n");    
+    printArr(a, n);  
+    return 0;  
+}  
+/*
+Before sorting array elements are -
+48 10 23 43 28 26 1
+After sorting array elements are -
+1 10 23 26 28 43 48
+--------------------------------/*
+
